d51ae86aaedda3b02f459c67597b28b9e64aef6b
Valerie R <vrosenhauer@gmail.com>
2024-01-04 01:12:18 -0500

    fixed comma in line 14

--- src/helpers.js
+++ src/helpers.js
@@ -1,20 +1,19 @@
 //randomly return selected item from array of items
 
 function choice(items) {
-    let idx = Math.floor(Math.random() * items.length);
-    return items[idx];
+  let idx = Math.floor(Math.random() * items.length);
+  return items[idx];
 }
 
-//remove first matching item form items, if item exists, and returns it. 
+//remove first matching item form items, if item exists, and returns it.
 // Otherwise returns undefined.
 
 function remove(item, items) {
-    for (let i=0; i < items.length; i++) {
-        if (items[i] === item) {
-            return [...items.slice(0, i) ...items.slice(i + 1)];
-        }
+  for (let i = 0; i < items.length; i++) {
+    if (items[i] === item) {
+      return [...items.slice(0, i), ...items.slice(i + 1)];
     }
+  }
 }
-
 
 export { choice, remove };
